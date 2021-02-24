0f5c85a5bbd3b634a391c45cd525a141ab4ca4fa
irma leandre <irmaleandre@irmas-MBP.PK5001Z>
2021-02-24 17:26:01 -0500

    Added 2nd Listening function

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 //const body = document.querySelector('body')
+let dcIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 58, 60, 66, 69, 70, 73, 76, 78, 81, 93, 95, 97, 98, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 142, 144, 150, 152, 156, 158, 165, 170, 172, 173, 174, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 229, 230, 233, 240, 242, 244, 246, 253, 255, 260, 261, 263, 276, 278, 284, 288, 294, 295, 298, 305, 306, 309, 312, 315, 316, 317, 318, 319, 320, 342, 348, 360, 367, 368, 370, 384, 386, 387, 388, 390, 396, 397, 405, 406, 407, 408, 413, 424, 427, 432, 436, 439, 444, 445, 446, 448, 451, 455, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 509, 514, 515, 516, 519, 520, 521, 522, 524, 528, 535, 538, 542, 543, 545, 551, 557, 558, 559, 564, 565, 569, 576, 585, 593, 597, 600, 601, 602, 603, 608, 609, 611, 615, 632, 633, 634, 635, 637, 641, 642, 643, 644, 645, 657, 674, 675, 676, 678, 692, 699, 706, 711, 713, 718, 720, 730, 731]
+let fIDArr = [52, 66, 73, 81, 97, 98, 142, 165, 172, 173, 174, 229, 242, 253, 255, 284, 295, 309, 315, 317, 318, 319, 342, 348, 360, 387, 406, 439, 444, 455, 516, 521, 522, 524, 542, 565, 585, 593, 597, 602, 603, 615, 632, 633, 643, 676, 699, 711, 718, 720, 730] //removes 596
+let mIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 53, 54, 55, 58, 60, 69, 70, 76, 78, 93, 95, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 144, 150, 152, 156, 158, 170, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 230, 233, 240, 244, 246, 260, 261, 263, 276, 278, 288, 294, 298, 305, 306, 312, 316, 320, 367, 368, 370, 384, 386, 388, 390, 396, 397, 405, 407, 408, 413, 424, 427, 432, 436, 445, 446, 448, 451, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 514, 515, 519, 520, 528, 535, 538, 543, 545, 551, 557, 558, 559, 564, 569, 576, 600, 601, 608, 609, 611, 634, 635, 637, 641, 642, 644, 645, 657, 674, 675, 678, 692, 706, 713, 731]
+let oIDArr = [509]
+let preference = []
+let character = {}
+
+let females = []
+let femHum = []
+let femHumGood = []
+let femHumBad = []
+let femOth = []
+let femOthGood = []
+let femOthBad = []
+
+
+let males = []
+let malHum = []
+let malHumGood = []
+let malHumBad = []
+let malOth = []
+let malOthGood = []
+let malOthBad = []
+
+let others = []
+let othGood = []
+let othBad = []
 
 const accessAPI = async (gender) => {
-
-  let dcIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 58, 60, 66, 69, 70, 73, 76, 78, 81, 93, 95, 97, 98, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 142, 144, 150, 152, 156, 158, 165, 170, 172, 173, 174, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 229, 230, 233, 240, 242, 244, 246, 253, 255, 260, 261, 263, 276, 278, 284, 288, 294, 295, 298, 305, 306, 309, 312, 315, 316, 317, 318, 319, 320, 342, 348, 360, 367, 368, 370, 384, 386, 387, 388, 390, 396, 397, 405, 406, 407, 408, 413, 424, 427, 432, 436, 439, 444, 445, 446, 448, 451, 455, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 509, 514, 515, 516, 519, 520, 521, 522, 524, 528, 535, 538, 542, 543, 545, 551, 557, 558, 559, 564, 565, 569, 576, 585, 593, 597, 600, 601, 602, 603, 608, 609, 611, 615, 632, 633, 634, 635, 637, 641, 642, 643, 644, 645, 657, 674, 675, 676, 678, 692, 699, 706, 711, 713, 718, 720, 730, 731]
-  let fIDArr = [52, 66, 73, 81, 97, 98, 142, 165, 172, 173, 174, 229, 242, 253, 255, 284, 295, 309, 315, 317, 318, 319, 342, 348, 360, 387, 406, 439, 444, 455, 516, 521, 522, 524, 542, 565, 585, 593, 597, 602, 603, 615, 632, 633, 643, 676, 699, 711, 718, 720, 730] //removes 596
-  let mIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 53, 54, 55, 58, 60, 69, 70, 76, 78, 93, 95, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 144, 150, 152, 156, 158, 170, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 230, 233, 240, 244, 246, 260, 261, 263, 276, 278, 288, 294, 298, 305, 306, 312, 316, 320, 367, 368, 370, 384, 386, 388, 390, 396, 397, 405, 407, 408, 413, 424, 427, 432, 436, 445, 446, 448, 451, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 514, 515, 519, 520, 528, 535, 538, 543, 545, 551, 557, 558, 559, 564, 569, 576, 600, 601, 608, 609, 611, 634, 635, 637, 641, 642, 644, 645, 657, 674, 675, 678, 692, 706, 713, 731]
-  let oIDArr = [509]
-  let preference = []
-  let character = {}
-
-  let females = []
-  let femHum = []
-  let femHumGood = []
-  let femHumBad = []
-  let femOth = []
-  let femOthGood = []
-  let femOthBad = []
-
-
-  let males = []
-  let malHum = []
-  let malHumGood = []
-  let malHumBad = []
-  let malOth = []
-  let malOthGood = []
-  let malOthBad = []
-
-  let other = []
-  let othGood = []
-  let othBad = []
   
 
   //console.log(dcIDArr.length) 
@@ -37,7 +36,7 @@ const accessAPI = async (gender) => {
 
   createFilter()
 
-  if (gender === 'female') {
+  /*if (gender === 'female') {
     preference = fIDArr
     //console.log(fIDArr)
   } else if (gender === 'male') {
@@ -46,13 +45,13 @@ const accessAPI = async (gender) => {
   } else {
     preference = oIDArr
     //console.log(oIDArr)
-  }
+  }*/
 
-  for (let id = 0; id < preference.length; id++) {
+  for (let id = 0; id < dcIDArr.length; id++) {
   
     const CORS = 'https://cors-anywhere.herokuapp.com/'
 
-    const marvelURL = `https://superheroapi.com/api/1967700243378120/${preference[id]}`
+    const marvelURL = `https://superheroapi.com/api/1967700243378120/${dcIDArr[id]}`
 
     try {
       const response = await axios.get(`${CORS}${marvelURL}`)
@@ -68,7 +67,8 @@ const accessAPI = async (gender) => {
       }*/
 
       profile(character)
-      //filtering(gen, character)
+      arrFiltering(gen, character)
+      //CON()
 
     } catch (err) {
       console.error(err)
@@ -85,7 +85,9 @@ const accessAPI = async (gender) => {
 
 //accessAPI()
 
-const filtering = (gen, character) => {
+
+//----------------------------------arrFiltering--------------------------------------//
+const arrFiltering = (gen, character) => {
   if (gen === 'Female') {
     females.push(character)
 
@@ -121,27 +123,29 @@ const filtering = (gen, character) => {
         malHumGood.push(character)
       }
     } else {
+      malOth.push(character)
       if (character.biography.alignment === 'good') {
-        other.push(character)
+        malOthGood.push(character)
+      } else if (character.biography.alignment === 'bad') {
+        malOthBad.push(character)
+      } else {
+        malOthGood.push(character)
+      }
+    }
+  } else {
+      others.push(character)
+    if (character.biography.alignment === 'good') {
+        othGood.push(character)
       } else if (character.biography.alignment === 'bad') {
         othBad.push(character)
       } else {
         othGood.push(character)
       }
-    }
-  } else {
-    oObjArr.push(character)
-    if (character.biography.alignment === 'good') {
-
-      } else if (character.biography.alignment === 'bad') {
-
-      } else {
-        
-      }
   }
+
 }
 
-//------------------------------------------------------------------------//
+//--------------------------------createFilter----------------------------------------//
 const createFilter = (person) => {
   const filterContainer = document.querySelector('#filter-container')
   const filterForm = document.createElement('form')
@@ -161,10 +165,34 @@ const createFilter = (person) => {
   const select = document.querySelector('#select-filter1')
   
 }
+//----------------------------------CON--------------------------------------//
 
 
+const CON = () => {
+  console.log(females[females.length - 1].id)
+  console.log(femHum[femHum.length - 1].id)
+  console.log(femHumGood[femHumGood.length - 1].id)
+  console.log(femHumBad[femHumBad.length - 1].id)
+  console.log('--------------')
+  console.log(femOth[femOth.length - 1].id)
+  console.log(femOthGood[femOthGood.length - 1].id)
+  console.log(femOthBad[femOthBad.length - 1].id)
+  console.log('--------------')
+  console.log(males[males.length - 1].id)
+  console.log(malHum[malHum.length - 1].id)
+  console.log(malHumGood[malHumGood.length - 1].id)
+  console.log(malHumBad[malHumBad.length - 1].id)
+  console.log('--------------')
+  console.log(malOth[malOth.length - 1].id)
+  console.log(malOthGood[malOthGood.length - 1].id)
+  console.log(malOthBad[malOthBAd.length - 1].id)
+  console.log('--------------')
+  console.log(others[others.length - 1].id)
+  console.log(othGood[othGood.length - 1].id)
+  console.log(othBad[othBad.length - 1].id)
+}
 
-//------------------------------------------------------------------------//
+//----------------------------------profile--------------------------------------//
 const profile = (person) => {
   console.log("Profile Info:", person)
   console.log('Inside profile')
@@ -186,61 +214,72 @@ const profile = (person) => {
   
 }
 
-//------------------------------------------------------------------------//
-const femaleButton = document.querySelector('#female')    
-//console.log(femaleButton)
-femaleButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  accessAPI('female')
-}) 
+//----------------------------------firstListen--------------------------------------//
 
-const maleButton = document.querySelector('#male')    
-//console.log(maleButton)
-maleButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  accessAPI('male')
-}) 
+const firstListen = () => {
+  const femaleButton = document.querySelector('#female')
+  //console.log(femaleButton)
+  femaleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    accessAPI('female')
+  })
 
-const otherButton = document.querySelector('#other')    
-//console.log(otherButton)
-otherButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  accessAPI('other')
-}) 
+  const maleButton = document.querySelector('#male')
+  //console.log(maleButton)
+  maleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    accessAPI('male')
+  })
 
+  const otherButton = document.querySelector('#other')
+  //console.log(otherButton)
+  otherButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    accessAPI('other')
+  })
+}
+//firstListen()
 
-/*
-const getData = async () => {
-  let marvelArr = []
-  let dcArr = []
-  
-  const CORS = 'https://cors-anywhere.herokuapp.com/'
-  
-  for (let i = 1; i < 732; i++) {
-    const marvelURL = `https://superheroapi.com/api/1967700243378120/${i}`
-  
-    try {
+//-----------------------------------listen-------------------------------------//
+const listen = () => {
+  const femaleButton = document.querySelector('#female')
+  //console.log(femaleButton)
+  femaleButton.addEventListener('click', (e) => {
+    e.preventDefault()
     
-      const response = await axios.get(`${CORS}${marvelURL}`)
-      const character = response.data
-      const publisher = response.data.biography.publisher
-      //console.log(response.data.name, publisher)
+    females.forEach((person) => {
+      profile(person)
+    })
+
+  })
+
+  const maleButton = document.querySelector('#male')
+  //console.log(maleButton)
+  maleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    males.forEach((person) => {
+      profile(person)
+    })
+
+  })
+
+  const otherButton = document.querySelector('#other')
+  //console.log(otherButton)
+  otherButton.addEventListener('click', (e) => {
+    e.preventDefault()
     
+    others.forEach((person) => {
+      profile(person)
+    })
+
+  })
+}
+//listen()
+
+//-----------------------------------filters-------------------------------------//
+
+const filters = () => {
   
-      if (publisher === 'Marvel Comics') {
-        marvelArr.push(character)
-      } else if (publisher === 'Superman Prime One-Million' || publisher === 'DC Comics') {
-        marvelArr.push(character)
-      }
-  
-    } catch (err) {
-      console.error(err)
-    }
-  
-    console.log(marvelArr)
-    console.log(dcArr)
-  }
-  
-  }
-  
-getData()*/
+}
+
