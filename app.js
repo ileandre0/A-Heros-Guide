aa442786571b3ebad41c7f9ad74848f5e7076019
irma leandre <irmaleandre@irmas-MBP.PK5001Z>
2021-02-25 09:09:28 -0500

    removeProfiles function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //const body = document.querySelector('body')
 let dcIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 58, 60, 66, 69, 70, 73, 76, 78, 81, 93, 95, 97, 98, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 142, 144, 150, 152, 156, 158, 165, 170, 172, 173, 174, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 229, 230, 233, 240, 242, 244, 246, 253, 255, 260, 261, 263, 276, 278, 284, 288, 294, 295, 298, 305, 306, 309, 312, 315, 316, 317, 318, 319, 320, 342, 348, 360, 367, 368, 370, 384, 386, 387, 388, 390, 396, 397, 405, 406, 407, 408, 413, 424, 427, 432, 436, 439, 444, 445, 446, 448, 451, 455, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 509, 514, 515, 516, 519, 520, 521, 522, 524, 528, 535, 538, 542, 543, 545, 551, 557, 558, 559, 564, 565, 569, 576, 585, 593, 597, 600, 601, 602, 603, 608, 609, 611, 615, 632, 633, 634, 635, 637, 641, 642, 643, 644, 645, 657, 674, 675, 676, 678, 692, 699, 706, 711, 713, 718, 720, 730, 731]
-let fIDArr = [52, 66, 73, 81, 97, 98, 142, 165, 172, 173, 174, 229, 242, 253, 255, 284, 295, 309, 315, 317, 318, 319, 342, 348, 360, 387, 406, 439, 444, 455, 516, 521, 522, 524, 542, 565, 585, 593, 597, 602, 603, 615, 632, 633, 643, 676, 699, 711, 718, 720, 730] //removes 596
+let fIDArr = [52, 66, 73, 81, 97, 98, 142, 165, 172, 173, 174, 229, 242, 253, 255, 284, 295, 309, 315, 317, 318, 319, 342, 348, 360, 387, 406, 439, 444, 455, 516, 521, 522, 524, 542, 565, 585, 593, 597, 602, 603, 615, 632, 633, 643, 676, 699, 711, 718, 720, 730] //removed 596
 let mIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 53, 54, 55, 58, 60, 69, 70, 76, 78, 93, 95, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 144, 150, 152, 156, 158, 170, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 230, 233, 240, 244, 246, 260, 261, 263, 276, 278, 288, 294, 298, 305, 306, 312, 316, 320, 367, 368, 370, 384, 386, 388, 390, 396, 397, 405, 407, 408, 413, 424, 427, 432, 436, 445, 446, 448, 451, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 514, 515, 519, 520, 528, 535, 538, 543, 545, 551, 557, 558, 559, 564, 569, 576, 600, 601, 608, 609, 611, 634, 635, 637, 641, 642, 644, 645, 657, 674, 675, 678, 692, 706, 713, 731]
 let oIDArr = [509]
 let preference = []
@@ -67,7 +67,7 @@ const accessAPI = async () => {
       //const character = response.data
       character = response.data
       //const publisher = response.data.biography.publisher
-      //const gen = response.data.appearance.gender
+      const gen = response.data.appearance.gender
       //console.log(response.data.name, publisher)
       //console.log(character, publisher)
 
@@ -156,6 +156,69 @@ const arrFiltering = (gen, character) => {
 
 }
 
+//-----------------------------------listen-------------------------------------//
+const listen = () => {
+
+  showFilters()
+
+  const femaleButton = document.querySelector('#female')
+  //console.log(femaleButton)
+  femaleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    removeProfiles()
+    showFilters()
+    filterValues()
+
+    females.forEach((person) => {
+      profile(person)
+    })
+
+  })
+
+  const maleButton = document.querySelector('#male')
+  //console.log(maleButton)
+  maleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    removeProfiles()
+    showFilters()
+    filterValues()
+
+    males.forEach((person) => {
+      profile(person)
+    })
+
+  })
+
+  const otherButton = document.querySelector('#other')
+  //console.log(otherButton)
+  otherButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    removeProfiles()
+    showFilters()
+    filterValues()
+
+    others.forEach((person) => {
+      profile(person)
+    })
+
+  })
+}
+listen()
+
+//-----------------------------------showFilters-------------------------------------//
+
+function showFilters() {                                                    //Rsource: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+  const show = document.querySelector('#filter-container');
+  if (show.style.display === "none") {
+    show.style.display = "block";
+  } else {
+    show.style.display = "none";
+  }
+}
+
 //----------------------------------filterValues--------------------------------------//
 
 const filterValues = () => {
@@ -180,6 +243,37 @@ const filterValues = () => {
     alignFilter.appendChild(option)
   })
 }
+
+//----------------------------------profile--------------------------------------//
+const profile = (person) => {
+  //console.log("Profile Info:", person)
+  //console.log('Inside profile')
+  const dataContainer = document.querySelector('#profile-container')
+  const profileInfo = document.createElement('div')
+  //console.log(dataContainer)
+
+  profileInfo.innerHTML = 
+    `<img src=${person.image.url} alt='${person.name} class='image'>
+    <h2> ${person.name} </h2>
+    <h3> ${person.work.base} </h3>
+    <div class='specs'>
+      <p> Hi. My name is ${person.name}, but you may know me as ${person.biography.aliases[0]}. I am ${person.appearance.race}, currently living in ${person.work.base}. I have an intelligence and power level of ${person.powerstats.intelligence}, and ${person.powerstats.power}, respectively. Take a look at my specs for more info.
+      <Interested? Contact me, if you can.
+    </div>`
+  
+  //console.log(profileInfo)
+  dataContainer.appendChild(profileInfo)
+  
+}
+
+//----------------------------------removeProfiles--------------------------------------//
+const removeProfiles = () => {
+  const removeDiv = document.querySelector('#profile-container')
+  while (removeDiv.lastChild) {
+    removeDiv.removeChild(RemoveDiv.lastChild)
+}
+}
+
 
 //----------------------------------CON--------------------------------------//
 
@@ -208,27 +302,7 @@ const CON = () => {
   console.log(othBad[othBad.length - 1].id)
 }
 
-//----------------------------------profile--------------------------------------//
-const profile = (person) => {
-  //console.log("Profile Info:", person)
-  //console.log('Inside profile')
-  const dataContainer = document.querySelector('#profile-container')
-  const profileInfo = document.createElement('div')
-  //console.log(dataContainer)
 
-  profileInfo.innerHTML = 
-    `<img src=${person.image.url} alt='${person.name} class='image'>
-    <h2> ${person.name} </h2>
-    <h3> ${person.work.base} </h3>
-    <div class='specs'>
-      <p> Hi. My name is ${person.name}, but you may know me as ${person.biography.aliases[0]}. I am ${person.appearance.race}, currently living in ${person.work.base}. I have an intelligence and power level of ${person.powerstats.intelligence}, and ${person.powerstats.power}, respectively. Take a look at my specs for more info.
-      <Interested? Contact me, if you can.
-    </div>`
-  
-  //console.log(profileInfo)
-  dataContainer.appendChild(profileInfo)
-  
-}
 //----------------------------------getData--------------------------------------//
 const getData = (e) => {
   e.preventDefault
@@ -239,65 +313,9 @@ const getData = (e) => {
   match.addEventListener('submit', )
 }//Finish here
 
-//-----------------------------------showFilters-------------------------------------//
 
-function showFilters() {                                                    //Rsource: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-  const show = document.querySelector('#filter-container');
-  if (show.style.display === "none") {
-    show.style.display = "block";
-  } else {
-    show.style.display = "none";
-  }
-}
 
-//-----------------------------------listen-------------------------------------//
-const listen = () => {
 
-  showFilters()
-
-  const femaleButton = document.querySelector('#female')
-  //console.log(femaleButton)
-  femaleButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    showFilters()
-    filterValues()
-
-    females.forEach((person) => {
-      profile(person)
-    })
-
-  })
-
-  const maleButton = document.querySelector('#male')
-  //console.log(maleButton)
-  maleButton.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    showFilters()
-    filterValues()
-
-    males.forEach((person) => {
-      profile(person)
-    })
-
-  })
-
-  const otherButton = document.querySelector('#other')
-  //console.log(otherButton)
-  otherButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    showFilters()
-    filterValues()
-
-    others.forEach((person) => {
-      profile(person)
-    })
-
-  })
-}
-listen()
 
 //-----------------------------------filters-------------------------------------//
 
