58f7d0e56ca85da87aed935add0477ee95c209bb
irma leandre <irmaleandre@irmas-MBP.PK5001Z>
2021-02-26 19:27:11 -0500

    loading Image completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let dcIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 58, 60, 66, 69, 70, 73, 76, 78, 81, 93, 95, 97, 98, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 142, 144, 150, 152, 156, 158, 165, 170, 172, 173, 174, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 229, 230, 233, 240, 242, 244, 246, 253, 255, 260, 261, 263, 276, 278, 284, 288, 294, 295, 298, 305, 306, 309, 312, 315, 316, 317, 318, 319, 320, 342, 348, 360, 367, 368, 370, 384, 386, 387, 388, 390, 396, 397, 405, 406, 407, 408, 413, 424, 427, 432, 436, 439, 444, 445, 446, 448, 451, 455, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 509, 514, 515, 516, 519, 520, 521, 522, 524, 528, 535, 538, 542, 543, 545, 551, 557, 558, 559, 564, 565, 569, 576, 585, 593, 597, 600, 601, 602, 603, 608, 609, 611, 615, 632, 633, 634, 635, 637, 641, 642, 643, 644, 645, 657, 674, 675, 676, 678, 692, 699, 706, 711, 713, 718, 720, 730, 731]
+let dcIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 58, 60, 66, 69, 70, 73, 76, 78, 81, 93, 95, 97, 98, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 142, 144, 150, 152, 156, 158, 165, 170, 172, 173, 174, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 229, 230, 233, 240, 242, 246, 253, 255, 260, 261, 263, 276, 278, 284, 288, 294, 295, 298, 305, 306, 309, 312, 315, 316, 317, 318, 319, 320, 342, 348, 360, 367, 368, 370, 384, 386, 387, 388, 390, 396, 397, 405, 406, 407, 408, 413, 424, 427, 432, 436, 439, 444, 445, 446, 448, 451, 455, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 509, 514, 515, 516, 519, 520, 521, 522, 524, 528, 535, 538, 542, 543, 545, 551, 557, 558, 559, 564, 565, 569, 576, 585, 593, 597, 600, 601, 602, 603, 608, 609, 611, 615, 632, 633, 634, 635, 637, 641, 642, 643, 644, 645, 657, 674, 675, 676, 678, 692, 699, 706, 711, 713, 718, 720, 730, 731]
 //let fIDArr = [52, 66, 73, 81, 97, 98, 142, 165, 172, 173, 174, 229, 242, 253, 255, 284, 295, 309, 315, 317, 318, 319, 342, 348, 360, 387, 406, 439, 444, 455, 516, 521, 522, 524, 542, 565, 585, 593, 597, 602, 603, 615, 632, 633, 643, 676, 699, 711, 718, 720, 730] //removed 596
-//let mIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 53, 54, 55, 58, 60, 69, 70, 76, 78, 93, 95, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 144, 150, 152, 156, 158, 170, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 230, 233, 240, 244, 246, 260, 261, 263, 276, 278, 288, 294, 298, 305, 306, 312, 316, 320, 367, 368, 370, 384, 386, 388, 390, 396, 397, 405, 407, 408, 413, 424, 427, 432, 436, 445, 446, 448, 451, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 514, 515, 519, 520, 528, 535, 538, 543, 545, 551, 557, 558, 559, 564, 569, 576, 600, 601, 608, 609, 611, 634, 635, 637, 641, 642, 644, 645, 657, 674, 675, 678, 692, 706, 713, 731]
+//let mIDArr = [3, 8, 14, 17, 20, 28, 32, 36, 37, 38, 49, 50, 51, 53, 54, 55, 58, 60, 69, 70, 76, 78, 93, 95, 100, 103, 105, 123, 124, 125, 126, 132, 136, 137, 144, 150, 152, 156, 158, 170, 177, 181, 184, 194, 195, 204, 212, 214, 216, 224, 230, 233, 240, 246, 260, 261, 263, 276, 278, 288, 294, 298, 305, 306, 312, 316, 320, 367, 368, 370, 384, 386, 388, 390, 396, 397, 405, 407, 408, 413, 424, 427, 432, 436, 445, 446, 448, 451, 457, 459, 461, 464, 466, 468, 494, 499, 506, 508, 514, 515, 519, 520, 528, 535, 538, 543, 545, 551, 557, 558, 559, 564, 569, 576, 600, 601, 608, 609, 611, 634, 635, 637, 641, 642, 644, 645, 657, 674, 675, 678, 692, 706, 713, 731] //removed: 244
 //let oIDArr = [509]
 
 let dcChar = []
@@ -10,24 +10,35 @@ let others = []
 let preference = []
 
 const accessAPI = async () => {                                           //async function to call API
-    //for (let id = 0; id < dcIDArr.length; id++) {
-      for (let id = 50; id < 71; id++) {                       //make a call to the API for each DC character in my array using their ID
+    for (let idElement = 1; idElement < dcIDArr.length; idElement++) {
+      //for (let idElement = 66; idElement !== 71; idElement++) {                       //make a call to the API for each DC character in my array using their ID
       
-      const CORS = 'https://cors-anywhere.herokuapp.com/'                 //Solve CORS error I had with accessing the API. Have to ask for permission.
+      //const CORS = 'https://cors-anywhere.herokuapp.com/'                 //Solve CORS error I had with accessing the API. Have to ask for permission.
 
-      const dcURL = `https://superheroapi.com/api/1967700243378120/${dcIDArr[id]}` //API URL
+      const dcURL = `https://superheroapi.com/api/1967700243378120/${dcIDArr[idElement]}` //API URL
 
-      try {
-        const response = await axios.get(`${CORS}${dcURL}`)               //Have to combine the CORS and DC URLs
+        try {
+        const response = await axios.get(dcURL)               //Have to combine the CORS and DC URLs
 
-        const show = document.querySelector('#loader');       //Resource: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-        while (id < 70) {
-          show.style.display = "block";
-        } 
-        // else {
-        //   show.style.display = "block";
-        // }
+        //const response = await axios.get(`${CORS}${dcURL}`)               //Have to combine the CORS and DC URLs
 
+          const show = document.querySelector('#loader');
+          const loadingMessage = document.createElement('div')
+                //Resource: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+          if (idElement !== dcIDArr.length - 1) {
+            show.style.display = "block"
+
+            if (show.lastChild) {
+              show.removeChild(show.lastChild)
+            }
+            
+            loadingMessage.innerHTML = '<h2>Calling all supers looking for love...</h2>'
+            show.appendChild(loadingMessage)
+
+            
+          } else {
+            show.style.display = "none"
+          }
 
         
         // const body = document.querySelector('body')
@@ -38,7 +49,7 @@ const accessAPI = async () => {                                           //asyn
         //   const loadingBar = document.querySelector('.loader')
         //   loadingImg.style.backgroundImage = "url('https://i.giphy.com/media/l3vRnoppYtfEbemBO/giphy.webp')"
         //   body.appendChild(loadingBar)
-        //   console.log('Calling all supers looking for love...')
+        
         // }
 
         let character = response.data          
@@ -98,7 +109,8 @@ const removeProfiles = () => {
 //-----------------------------------getFilterValues-------------------------------------//
 
 function getFilterValues(e) {
-    e.preventDefault()
+  e.preventDefault()
+  listen()
     console.log('match')
     const raceValue = document.querySelector('#select-rFilter').value
     const alignmentValue = document.querySelector('#select-aFilter').value
@@ -167,7 +179,7 @@ function getFilterValues(e) {
     //   console.log('4', char.appearance.race)
     // }
 
-    listen()
+    
       console.log('in filters')
     })
 }
@@ -232,7 +244,13 @@ function listen() {
 //-----------------------------------showFilters-------------------------------------//
 
 function showFilters() {
+
   const filterContainer = document.querySelector('#filter-container')
+
+  while (filterContainer.lastChild) {
+    filterContainer.removeChild(filterContainer.lastChild)
+  }
+
   if (filterContainer.lastChild === null) {
     let filters = `
       <p class='text'>Filters:</p>
